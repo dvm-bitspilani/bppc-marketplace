083af3de42373a9d753bcf59f339ece74c707d99
Akshit Patel <akshitpatel72@gmail.com>
2019-07-08 02:06:31 +0530

    user dropdown

--- src/App.js
+++ src/App.js
@@ -8,16 +8,7 @@ import SignUp from "./views/Signup";
 import Dashboard from "./views/dashboard";
 import Buy from "./views/buy";
 import Sell from "./views/sell";
-
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink} from 'reactstrap';
-
+import { NavItem, NavLink ,Dropdown, DropdownItem, DropdownToggle, DropdownMenu} from 'reactstrap';
 import styles from "./header.module.css";
 
 function HomepageLinks(){ 
@@ -32,17 +23,46 @@ function HomepageLinks(){
     </div>
   );
 }
-function DashboardLinks(){ 
-return (
-<div style={{display:"flex"}}>
-  <NavItem>
-  <NavLink href="/buy" className={styles.link}> Buy</NavLink>
-  </NavItem>
-  <NavItem>
-  <NavLink href="/sell" className={styles.link}>Sell</NavLink>
-  </NavItem>
-</div>
-  );  
+class DashboardLinks extends React.Component{ 
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  render(){
+    return (
+      <div style={{display:"flex"}}>
+        <NavItem>
+        <NavLink href="/buy" className={styles.link}> Buy</NavLink>
+        </NavItem>
+        <NavItem>
+        <NavLink href="/sell" className={styles.link}>Sell</NavLink>
+        </NavItem>
+        <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                  <DropdownToggle nav caret className={styles.dropdown} >
+                    Hi, user
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem >Status</DropdownItem>
+                    {/* <DropdownItem disabled>Selling</DropdownItem> */}
+                    <DropdownItem> Notifications</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem header>settings</DropdownItem>
+                  </DropdownMenu>
+        </Dropdown>
+      </div>
+  );
+  }  
 }
 
 function App() {
