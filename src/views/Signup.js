35c457edc69098ceceb359d00039d86bd140ce08
meliodas113 <f20180433@pilani.bits-pilani.ac.in>
2019-07-08 15:08:58 +0530

    form validation for login done

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,58 +1,174 @@
-import React, { Component } from 'react';
-import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Col,
+  Container,
+  Form,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Row
+} from "reactstrap";
+import axios from "axios";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  repeatpassword: "",
+  usernameError: "",
+  emailError: "",
+  passwordError: "",
+  repeatpasswordError: ""
+};
 
 class Register extends Component {
+  state = initialState;
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  validate = () => {
+    let usernameError = "";
+    let emailError = "";
+    let passwordError = "";
+    let repeatpasswordError = "";
+
+    if (!this.state.username) {
+      usernameError = "username cannot be blank";
+    }
+
+    if (!this.state.email) {
+      emailError = "email cannot be blank";
+    }
+
+    if (!this.state.password) {
+      passwordError = "password cannot be blank";
+    }
+
+    if (this.state.password !== this.state.repeatpassword) {
+      repeatpasswordError = "must be same";
+      this.setState(repeatpasswordError);
+      return false;
+    }
+
+    if (!this.state.email.includes("@")) {
+      emailError = "invalid email";
+    }
+
+    if (emailError || usernameError || passwordError) {
+      this.setState({ emailError, usernameError, passwordError });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state);
+      axios
+        .post("url", this.state)
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      // clear form
+      this.setState(initialState);
+    }
+  };
   render() {
     return (
-      <div className="app flex-row align-items-center" >
+      <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
             <Col md="9" lg="7" xl="6">
               <Card className="mx-4">
                 <CardBody className="p-4">
-                  <Form>
+                  <Form onSubmit={this.handleSubmit}>
                     <h1>Register</h1>
                     <p className="text-muted">Create your account</p>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
-                          <i className="icon-user"></i>
+                          <i className="icon-user" />
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Username" autoComplete="username" />
+                      <Input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        autoComplete="username"
+                        onChange={this.handleChange}
+                      />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>@</InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Email" autoComplete="email" />
+                      <Input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        autoComplete="email"
+                        onChange={this.handleChange}
+                      />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
-                          <i className="icon-lock"></i>
+                          <i className="icon-lock" />
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                      <Input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        autoComplete="new-password"
+                        onChange={this.handleChange}
+                      />
                     </InputGroup>
                     <InputGroup className="mb-4">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
-                          <i className="icon-lock"></i>
+                          <i className="icon-lock" />
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+                      <Input
+                        type="password"
+                        name="repeatpassword"
+                        placeholder="Repeat password"
+                        autoComplete="new-password"
+                        onChange={this.handleChange}
+                      />
                     </InputGroup>
-                    <Button color="success" block>Create Account</Button>
+                    <Button color="success" block onClick={this.handleSubmit}>
+                      Create Account
+                    </Button>
                   </Form>
                 </CardBody>
                 <CardFooter className="p-4">
                   <Row>
                     <Col xs="12" sm="6">
-                      <Button className="btn-facebook mb-1" block><span>facebook</span></Button>
+                      <Button className="btn-facebook mb-1" block>
+                        <span>facebook</span>
+                      </Button>
                     </Col>
                     <Col xs="12" sm="6">
-                      <Button className="btn-twitter mb-1" block><span>twitter</span></Button>
+                      <Button className="btn-twitter mb-1" block>
+                        <span>twitter</span>
+                      </Button>
                     </Col>
                   </Row>
                 </CardFooter>
