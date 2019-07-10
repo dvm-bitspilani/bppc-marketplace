487029f1519ca40e931269108fea089d3ae13410
Shreyans Jain <shreyansj551@gmail.com>
2019-07-10 19:37:32 +0530

    basic modal functional

--- src/views/buy.js
+++ src/views/buy.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
-import { Router, Link } from "react-router-dom";
+// import { Router, Link } from "react-router-dom";
 
-import {Table} from 'reactstrap';
+import { Table } from "reactstrap";
+import Modal from "../components/Modal";
+
 class Buy extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      showForm: false,
-      showFullYearForm: false
+      showModal: false
     };
+  }
+
+  showModal = () => {
+    console.log('modal shown')
+    this.setState({
+      showModal: true 
+    })
+  }
+
+  hideModal = () => {
+    console.log('modal hidden')
+    this.setState({
+      showModal: false
+    })
   }
 
   render() {
@@ -27,15 +42,28 @@ class Buy extends Component {
           </thead>
           <tbody>
             <tr>
-              <td><img src="../assets/avatars/tn.jpg" height="100" width="100"></img></td>
-              <td>Johny English</td>
+              <td>
+                <img src="../assets/avatars/tn.jpg" height="100" width="100" />
+              </td>
+              <td onClick={() => this.showModal()}>Johny English</td>
+              <td>3500</td>
+              <td>7/10</td>
+            </tr>
+            <tr>
+              <td>
+                <img src="../assets/avatars/tn.jpg" height="100" width="100" />
+              </td>
+              <td onClick={() => this.showModal()}>Johny English</td>
               <td>3500</td>
               <td>7/10</td>
             </tr>
           </tbody>
         </Table>
+        <Modal show={this.state.showModal}>
+          <div className="close" onClick={() => this.hideModal()}>Close</div>
+        </Modal>
       </div>
-    )
+    );
   }
 }
 
