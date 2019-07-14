de905e2778bb60133f78d1038d31a5e7f972853c
Shreyans Jain <shreyansj551@gmail.com>
2019-07-14 22:20:20 +0530

    basic auth action done

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,16 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  token: null,
+  userId: null,
+  name: null,
+  email: null
+}
+
+const reducer = (state,reducer) => {
+  switch(actionTypes.type) {
+    
+  }
+}
+
+export default reducer;
