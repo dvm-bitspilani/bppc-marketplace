5b6d29ea85888a3897ce8deb47fd825b3444478a
Shreyans Jain <shreyansj551@gmail.com>
2019-07-16 16:42:24 +0530

    fix:wrap app with Provider

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ const app = (
   </Provider>
 )
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
