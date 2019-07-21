9fc6f7e490c48101bbb0782d45855340e184861b
Shreyans Jain <shreyansj551@gmail.com>
2019-07-21 22:28:11 +0530

    style buy modal

--- src/components/Buy/SellerSummary/SellerSummary.js
+++ src/components/Buy/SellerSummary/SellerSummary.js
@@ -1,41 +1,60 @@
 import React from "react";
 
-import BookCarousel from '../Modal/Carousel';
-// const carousel_items = [
-//   {
-//     src: "https://picsum.photos/300",
-//     altText: "Image 1",
-//     caption: "Book 1",
-//   },
-//   {
-//     src: "https://picsum.photos/300",
-//     altText: "Image 1",
-//     caption: "Book 1",
-//   },
-//   {
-//     src: "https://picsum.photos/300",
-//     altText: "Image 1",
-//     caption: "Book 1",
-//   }
-// ]
+import BookCarousel from "../Modal/Carousel";
+import "./SellerSummary.css";
 
-const SellerSummary = props => {
-  return (
-    <div>
-      <h1>Seller Details</h1>
-      <p>Name of Seller: {props.seller.name}</p>
-      <p>Price: Rs.{props.seller.price}</p>
-      <p>Number of books available: {props.seller.numBooks} out of 10</p>
-      <BookCarousel />
-      <br />
-      <p> List of all books available with {props.seller.name}: </p>
-      <ul>
-        <li>Thermodynics Course book</li>
-        <li>Gen Bio course book</li>
-        <li>CP reference book</li>
-      </ul>
-    </div>
-  );
-};
+// const SellerSummary = props => {
+
+class SellerSummary extends React.Component {
+  state = {
+    tags: ["new condition", "thermo table"]
+  };
+
+  render() {
+    const tagList = this.state.tags.map((tag, index) => {
+      return <span key={index} className="tag">{tag}</span>;
+    });
+
+    return (
+      <div className="Summary">
+        <h1>Seller Details</h1>
+
+        <table className="summaryTable">
+          <tbody>
+            <tr>
+              <td>Name of Seller:</td>
+              <td>{this.props.seller.name}</td>
+            </tr>
+            <tr>
+              <td>Price:</td>
+              <td>{this.props.seller.price}</td>
+            </tr>
+            <tr>
+              <td>Number of books available:</td>
+              <td>{this.props.seller.numBooks}</td>
+            </tr>
+          </tbody>
+        </table>
+        <hr />
+        <div className="tags">
+          <div className="tags-header">Tags:</div>
+          <div className="tags-div">
+            {tagList}
+          </div>
+        </div>
+
+        <BookCarousel />
+
+        <br />
+        <p> List of all books available with {this.props.seller.name}: </p>
+        <ul>
+          <li>Thermodynics Course book</li>
+          <li>Gen Bio course book</li>
+          <li>CP reference book</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default SellerSummary;
