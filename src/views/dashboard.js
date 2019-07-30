a34758211b0f1403e70eec363cdde34d2c4e3cc2
meliodas113 <f20180433@pilani.bits-pilani.ac.in>
2019-07-30 21:44:54 +0530

    dashboard updated

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { navigate } from '@reach/router'
+import React, { Component } from "react";
+import { navigate } from "@reach/router";
+import { Container, Jumbotron, ListGroup, ListGroupItem } from "reactstrap";
 
 export class dashboard extends Component {
   componentDidMount() {
@@ -7,15 +8,56 @@ export class dashboard extends Component {
       window.alert("Unauthenticated user. Please login first!");
       setTimeout(() => navigate("/login"), 100);
     }
-   }
+  }
 
   render() {
     return (
       <div>
-        Dashboard
+        <Container>
+          <Jumbotron>
+            <h3 className="display-3">General Instructions</h3>
+            <ListGroup style={{ marginTop: "5vh", marginBottom: "5vh" }} active>
+              <ListGroupItem active action>
+                Selling Books
+              </ListGroupItem>
+              <ListGroupItem>
+                Click on the sell button on the top navigation bar.
+              </ListGroupItem>
+              <ListGroupItem>Select the books which you have.</ListGroupItem>
+              <ListGroupItem>
+                Upload the pictures of the books/notes or anything extra that
+                you are providing to increase your selling chances.(Increases
+                your credibility)
+              </ListGroupItem>
+              <ListGroupItem>
+                Describe that extra material (if any) that you are providing.
+                (You can use upto 5, one word tags too to shortly grab attention
+                in a jist.)
+              </ListGroupItem>
+            </ListGroup>
+            <hr className="my-2" />
+            <ListGroup style={{ marginTop: "5vh", marginBottom: "5vh" }} active>
+              <ListGroupItem active action>
+                Buying Books
+              </ListGroupItem>
+              <ListGroupItem>
+                Click on the Buy button on the top navigation bar.
+              </ListGroupItem>
+              <ListGroupItem>
+                The page lists all the sellers that are available with their
+                expected price and all the details of books.
+              </ListGroupItem>
+              <ListGroupItem>
+                You can click on the details to see the conditions of the books
+                of that particular seller, and any other extra material
+                provided.
+              </ListGroupItem>
+            </ListGroup>
+          </Jumbotron>
+        </Container>
       </div>
-    )
+    );
   }
 }
 
-export default dashboard
+export default dashboard;
